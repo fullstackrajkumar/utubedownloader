c425736b4c4a5065ba27f5146f3f9fd2d332c7b8
Rajkumar <personal.prince.yd@gmail.com>
2021-07-26 15:59:34 +0530

    this projects first commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = new express();
+const ytdl = require('ytdl-core');
+const port = process.env.PORT || 3000;
+
+app.get("/", (req,res)=>{
+    res.send("Hello this is working...");
+});
+
+app.get("/fetch", async (req,res)=>{
+    try{
+        const vid = await ytdl.getURLVideoID(req.query.url);
+        let info = await ytdl.getInfo(vid);
+        const respondArray = info.player_response.streamingData.formats;
+        const narray = [];
+        for(let i=0;i<respondArray.length;i++){
+            if(respondArray[i].itag === 18){
+                narray[i] = {
+                    "itag" : respondArray[i].itag,
+                    "quality" : "360p",
+                    "url" : respondArray[i].url
+                }
+            }else if(respondArray[i].itag === 22){
+                narray[i] = {
+                    "itag" : respondArray[i].itag,
+                    "quality" : "720p",
+                    "url" : respondArray[i].url
+                }
+            }else if(respondArray[i].itag === 37){
+                narray[i] = {
+                    "itag" : respondArray[i].itag,
+                    "quality" : "1080p",
+                    "url" : respondArray[i].url
+                }
+            }else if(respondArray[i].itag === 38){
+                narray[i] = {
+                    "itag" : respondArray[i].itag,
+                    "quality" : "3072p",
+                    "url" : respondArray[i].url
+                }
+            }
+        }
+        res.status(200).send(narray);
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
+
+app.listen(port,()=>{
+    console.log("server is running...");
+});
